bd3fc138afc9d577e5d36bed325d00b2a7af370e
Mission Bit <missionbit@127p-MissionBit.local>
2020-10-26 16:39:08 -0700

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+#dictionary
+
+#name_of_dictionary = {key : value key : value key: value,...}
+car_information = {"Brand": "Honda",
+                   "Model": "Civic",
+                   "Year": "2017",
+                   "Color": "Red"}
+print(car_information)
+
+
+#user info
+user_information = {"21jalynm" : "21jalynm@gateway-students.net", 
+                    "jalyn2409" : "jalyneva23@gmail.com",
+                    "jalynmc" : "jalyneva234@gmail.com"}
+print(user_information)
